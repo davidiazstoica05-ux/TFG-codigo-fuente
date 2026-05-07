bb7f7bdc5d2a80e87ecec61941e11ed68e9d95d0
davidiazstoica05-ux <davidiazstoica05@gmail.com>
2026-05-07 16:21:03 +0200

    ·first commit

---
+++ City-Courier/src/main/java/com/tfg_david/dam/City_Courier/CityCourierApplication.java
@@ -0,0 +1,13 @@
+package com.tfg_david.dam.City_Courier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityCourierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CityCourierApplication.class, args);
+	}
+
+}
---
+++ City-Courier/src/test/java/com/tfg_david/dam/City_Courier/CityCourierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tfg_david.dam.City_Courier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityCourierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
