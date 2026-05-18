c800bb60a7750c260cb33e7b8112d83d1041497f
davidiazstoica05-ux <davidiazstoica05@gmail.com>
2026-05-18 18:54:40 +0200

    fe:(Creacion de findById) Se crea una funcion findById en el repositorio de cliente, se modifica el controlador para que use esta funcion y se crean las vistas para mostrar los datos del cliente.

--- City-Courier/src/main/java/com/tfg_david/dam/City_Courier/controller/RepartidorController.java
+++ City-Courier/src/main/java/com/tfg_david/dam/City_Courier/controller/RepartidorController.java
@@ -1,6 +1,8 @@
 package com.tfg_david.dam.City_Courier.controller;
 
+import java.util.HashSet;
 import java.util.Optional;
+import java.util.Set;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,75 +11,84 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.tfg_david.dam.City_Courier.model.Repartidor;
 import com.tfg_david.dam.City_Courier.service.RepartidorService;
 
 import lombok.RequiredArgsConstructor;
 
-
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/rrhh") 
+@RequestMapping("/rrhh")
 public class RepartidorController {
 
-    private final RepartidorService repartidorService;
-	
-    @GetMapping("/repartidores")
-    public String rrhh(Model model) {
-        model.addAttribute("repartidorList", repartidorService.findAll());
-        
-        model.addAttribute("repartidor", new Repartidor());
-        
-        return "rrhh"; 
-    }
+	private final RepartidorService repartidorService;
 
-    @GetMapping("/editar/{dni}")
-	public String editarRepartidor(@PathVariable("dni") String dni, Model model) {
-    	
-    	Optional<Repartidor> repartidor = repartidorService.findById(dni); 
-    	
-    
-    	if (repartidor.isPresent()) {
-    		
-    		model.addAttribute("repartidor", repartidor.get());
-    		
-    		model.addAttribute("repartidorList", repartidorService.findAll());
-            
-    		model.addAttribute("modoEdicion", true);
-    		
-    		return "rrhh"; 
-			
+	@GetMapping("/repartidores")
+	public String rrhh(@RequestParam(required = false) String dni, Model model) {
+
+		if (dni != null && !dni.isEmpty() ) {
+
+			Optional<Repartidor> repartidor = repartidorService.findById(dni);
+
+			if (repartidor.isPresent()) {
+				
+				Set<Repartidor> r = new HashSet<>();
+				r.add(repartidor.get()); 
+
+				model.addAttribute("repartidorList", r);
+				
+
+			} else {
+
+				return "redirect: /rrhh/repartidores";
+
+			}
+
 		} else {
-			
-			return "redirect:/rrhh/repartidores";
-			
-		}
-    }
-	
-    
-    @GetMapping("/findByDNI/{dni}")
-    public String findByDNIRepartidor(@PathVariable("dni") String dni, Model model) {
-    	
-    	Optional<Repartidor> repartidor = repartidorService.findById(dni);
-    	
-    	if (repartidor.isPresent()) {
-			
-    		model.addAttribute("repartidor",repartidor.get());
-    		
-		}
-    	
-    	
-    }
-	
-	@PostMapping("/createRider/submit")
-    public String submit (@ModelAttribute("repartidor") Repartidor repartidor, Model model ) {
-        repartidorService.save(repartidor);
-        return "redirect:/rrhh/repartidores";
-    }
-	
-	
 
-	
-	
+			model.addAttribute("repartidorList", repartidorService.findAll());
+
+
+		}
+		
+		
+		model.addAttribute("repartidor", new Repartidor());
+		
+		return "rrhh";
+
+
+	}
+
+	@GetMapping("/editar/{dni}")
+	public String editarRepartidor(@PathVariable("dni") String dni, Model model) {
+
+		Optional<Repartidor> repartidor = repartidorService.findById(dni);
+
+		if (repartidor.isPresent()) {
+
+			model.addAttribute("repartidor", repartidor.get());
+
+			model.addAttribute("repartidorList", repartidorService.findAll());
+
+			model.addAttribute("modoEdicion", true);
+
+			return "rrhh";
+
+		} else {
+
+			return "redirect: /rrhh/repartidores";
+
+		}
+	}
+
+
+
+	@PostMapping("/repartidores")
+	public String submit(@ModelAttribute("repartidor") Repartidor repartidor, Model model) {
+		repartidorService.save(repartidor);
+		return "redirect: /rrhh/repartidores";
+	}
+
 }
