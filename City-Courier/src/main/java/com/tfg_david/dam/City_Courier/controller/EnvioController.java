ff90f308899e91548697d179a00d7a5ffb837838
davidiazstoica05-ux <davidiazstoica05@gmail.com>
2026-05-19 20:57:25 +0200

    first commit, revisar error

--- City-Courier/src/main/java/com/tfg_david/dam/City_Courier/controller/EnvioController.java
+++ City-Courier/src/main/java/com/tfg_david/dam/City_Courier/controller/EnvioController.java
@@ -1,9 +1,12 @@
 package com.tfg_david.dam.City_Courier.controller;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -15,7 +18,7 @@ import lombok.RequiredArgsConstructor;
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/logistica")
-public class LogisticaController {
+public class EnvioController {
 
 	private final EnviosService envioService;
 
@@ -37,5 +40,27 @@ public class LogisticaController {
 		return "redirect:/logistica/principal";
 
 	}
+	
+	@GetMapping("/editar/{codEnvio}")
+	public String editarEnvio(@PathVariable("codEnvio") Long codEnvio, Model model) {
+		
+		Optional<Envio> envio = envioService.findById(codEnvio); 
+		
+		if (envio.isPresent()) {
+			
+			model.addAttribute("envio", envio.get()); 
+			model.addAttribute("envioList", envioService.findAll());  
+			model.addAttribute("modoEdicion",true); 
+			return "logistica/logistica";
+		}else {
+			
+			return "redirect:/logistica/principal";
+		}
+				
+	}
+	
 
+	
+	
+	
 }
