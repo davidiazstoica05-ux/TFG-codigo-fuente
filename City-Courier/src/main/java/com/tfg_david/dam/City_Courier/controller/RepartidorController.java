ebdd148117d05363fe5e5fb5e38f75722e9bc45a
davidiazstoica05-ux <davidiazstoica05@gmail.com>
2026-05-20 16:44:29 +0200

    fe: FindByID para envios Se ha implemetado y creado correctamente el método findByID para envíos

--- City-Courier/src/main/java/com/tfg_david/dam/City_Courier/controller/RepartidorController.java
+++ City-Courier/src/main/java/com/tfg_david/dam/City_Courier/controller/RepartidorController.java
@@ -28,13 +28,14 @@ public class RepartidorController {
 	@GetMapping("/repartidores")
 	public String rrhh(@RequestParam(required = false) String dni, Model model) {
 
+		Set<Repartidor> r = new HashSet<>();
+		
 		if (dni != null && !dni.isEmpty() ) {
 
 			Optional<Repartidor> repartidor = repartidorService.findById(dni);
 
 			if (repartidor.isPresent()) {
 				
-				Set<Repartidor> r = new HashSet<>();
 				r.add(repartidor.get()); 
 
 				model.addAttribute("repartidorList", r);
@@ -42,7 +43,7 @@ public class RepartidorController {
 
 			} else {
 
-				return "redirect: /rrhh/repartidores";
+				return "redirect:/rrhh/repartidores";
 
 			}
 
