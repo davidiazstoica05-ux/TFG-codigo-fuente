b512cccfb77e7dccf73a5745bd93a3d646738db1
davidiazstoica05-ux <davidiazstoica05@gmail.com>
2026-05-20 21:13:25 +0200

    ·COntroladores y formulario creados

--- City-Courier/src/main/java/com/tfg_david/dam/City_Courier/controller/RutaController.java
+++ City-Courier/src/main/java/com/tfg_david/dam/City_Courier/controller/RutaController.java
@@ -3,8 +3,11 @@ package com.tfg_david.dam.City_Courier.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.tfg_david.dam.City_Courier.model.Ruta;
 import com.tfg_david.dam.City_Courier.service.RutaService;
 
 import lombok.RequiredArgsConstructor;
@@ -22,9 +25,21 @@ public class RutaController {
 		
 		model.addAttribute("rutaList", rutaService.findAll());
 		
+		model.addAttribute("ruta", new Ruta());
 		
 		return "logistica/rutas"; 
 	}
+	
+	@PostMapping("/rutas")
+	public String submit(@ModelAttribute("ruta") Ruta ruta, Model model) {
+		
+		rutaService.save(ruta);
+		
+		return "redirect:/logistica/rutas";
+		
+	}
+	
+	
 	
 
 }
