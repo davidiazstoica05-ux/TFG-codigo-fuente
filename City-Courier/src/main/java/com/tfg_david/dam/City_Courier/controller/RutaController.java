b8880b160de036e5657526df79407f298bb85894
davidiazstoica05-ux <davidiazstoica05@gmail.com>
2026-05-21 18:09:47 +0200

    Creacion del editor de rutas

--- City-Courier/src/main/java/com/tfg_david/dam/City_Courier/controller/RutaController.java
+++ City-Courier/src/main/java/com/tfg_david/dam/City_Courier/controller/RutaController.java
@@ -3,16 +3,18 @@ package com.tfg_david.dam.City_Courier.controller;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.tfg_david.dam.City_Courier.model.Ruta;
+ import com.tfg_david.dam.City_Courier.model.Ruta;
 import com.tfg_david.dam.City_Courier.service.RutaService;
 
 import lombok.RequiredArgsConstructor;
@@ -49,6 +51,25 @@ public class RutaController {
 		
 		return "redirect:/logistica/rutas";
 		
+	}
+	
+	@GetMapping("/rutas/{codigoRuta}")
+	public String editarRuta(@PathVariable("codigoRuta") Long codigoRuta, Model model) {
+
+		Optional<Ruta> ruta = rutaService.findById(codigoRuta);
+
+		if (ruta.isPresent()) {
+
+			model.addAttribute("ruta", ruta.get());
+			model.addAttribute("rutaList", rutaService.findAll());
+			model.addAttribute("modoEdicion", true);
+			return "logistica/rutas";
+
+		} else {
+
+			return "redirect:/logistica/rutas";
+		}
+
 	}
 	
 	
