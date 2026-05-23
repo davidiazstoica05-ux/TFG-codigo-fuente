07932b2f9dec5e2fd659092d18305b8b871e405c
davidiazstoica05-ux <davidiazstoica05@gmail.com>
2026-05-23 12:37:58 +0200

    feat(implementar form agregar): se ha implementado el form que permite haer una nueva asociacion
    Se tuvierón que realizar algunas funcionalidades para que las entidades se enteraran de los cambios

--- City-Courier/src/main/java/com/tfg_david/dam/City_Courier/controller/AsignacionController.java
+++ City-Courier/src/main/java/com/tfg_david/dam/City_Courier/controller/AsignacionController.java
@@ -3,9 +3,15 @@ package com.tfg_david.dam.City_Courier.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.tfg_david.dam.City_Courier.model.Asignacion;
+import com.tfg_david.dam.City_Courier.model.Repartidor;
 import com.tfg_david.dam.City_Courier.service.AsignacionService;
+import com.tfg_david.dam.City_Courier.service.EnviosService;
+import com.tfg_david.dam.City_Courier.service.RepartidorService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -13,20 +19,40 @@ import lombok.RequiredArgsConstructor;
 @Controller
 @RequestMapping("/logistica")
 public class AsignacionController {
-	
-	private final AsignacionService asigService; 
+
+	private final AsignacionService asigService;
+	private final EnviosService envioService; 
+	private final RepartidorService repartidorService;
 
 	@GetMapping("/asignaciones")
-	public String asignacion( Model model) {
+	public String asignacion(Model model) {
 		
+
 		model.addAttribute("asignacionList", asigService.findAll());
+
+		model.addAttribute("asignacion", new Asignacion());
 		
+		model.addAttribute("repartidoresList", repartidorService.findAll());
+
+		model.addAttribute("enviosList",envioService.findAll());
+
 		
-		return "logistica/asignaciones"; 
-		
+		return "logistica/asignaciones";
+
 	}
-	
-	
-	
-	
+
+	@PostMapping("/asignaciones")
+	public String asignaciones(@ModelAttribute("asignacion") Asignacion asignacion, Model model) {
+
+		if (asigService.asignarRepartidor(asignacion) && asigService.asignarEnvio(asignacion)) {
+						
+			
+			asigService.save(asignacion);
+
+		}
+
+		return "redirect:/logistica/asignaciones";
+
+	}
+
 }
--- City-Courier/src/main/java/com/tfg_david/dam/City_Courier/model/Asignacion.java
+++ City-Courier/src/main/java/com/tfg_david/dam/City_Courier/model/Asignacion.java
@@ -10,6 +10,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
+import jakarta.persistence.PrePersist;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,45 +18,56 @@ import lombok.NoArgsConstructor;
 
 @Entity
 @Data
-@AllArgsConstructor @NoArgsConstructor
+@AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class Asignacion {
 
-	@Id 
+	@Id
 	@GeneratedValue
 	private Long idAsignacion;
-	private boolean estadoPedido; 
-	private double coste; 
-	
-	private LocalDateTime fechaAsignacion; 
-	private LocalDateTime fechaEntrega; 
-	
+	private boolean estadoPedido;
+	private double coste;
+
+	private LocalDateTime fechaAsignacion;
+	private LocalDateTime fechaEntrega;
+
 	private String motivoIncidencia;
-	
-	@ManyToOne 
-	@JoinColumn(name = "dni")
-	private Repartidor repartidor; 
-	
+
 	@ManyToOne
-	@JoinColumn(name = "codigoRuta")
-	private Ruta ruta;
-	
+	@JoinColumn(name = "dni")
+	private Repartidor repartidor;
+
 	@OneToOne(mappedBy = "asignacion", cascade = CascadeType.ALL)
 	private Envio envio;
-	
+
 	private Duration tiempoEstimado; // fechaInicio-fechaFinal; acordarse hacerla en service
-	
-	
+
 	public void vincularEnvio(Envio envio) {
-		
+
 		this.envio = envio;
-		
+
 		if (envio != null) {
-			
+
 			envio.setAsignacion(this);
+
+		}
+
+	}
+
+	@PrePersist
+	public void configurarHoraActural() {
+		
+		
+		if (fechaAsignacion == null) {
+			
+			
+			fechaAsignacion = LocalDateTime.now();
 			
 		}
 		
+		
+		
 	}
-	
+
 }
