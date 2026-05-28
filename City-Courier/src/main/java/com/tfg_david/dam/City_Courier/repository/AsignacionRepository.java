1461b3a189affb7501047b5da373d49773a08a2f
David Díaz Stoica <diaz.stdav24@triana.salesianos.edu>
2026-05-28 09:53:24 +0200

    fe:deleteRepartidor

--- City-Courier/src/main/java/com/tfg_david/dam/City_Courier/repository/AsignacionRepository.java
+++ City-Courier/src/main/java/com/tfg_david/dam/City_Courier/repository/AsignacionRepository.java
@@ -5,11 +5,14 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.tfg_david.dam.City_Courier.model.Asignacion;
+import com.tfg_david.dam.City_Courier.model.Repartidor;
 
 public interface AsignacionRepository extends JpaRepository<Asignacion, Long> {
 
 	List<Asignacion> findByIdAsignacionOrRepartidorDni( Long idAsignacion, String dni);
 
 	Long countByEstadoPedido(boolean estado);
+	
+	List<Asignacion> findByRepartidor(Repartidor repartidor);
 	
 }
--- City-Courier/src/main/java/com/tfg_david/dam/City_Courier/service/RepartidorService.java
+++ City-Courier/src/main/java/com/tfg_david/dam/City_Courier/service/RepartidorService.java
@@ -1,18 +1,27 @@
 package com.tfg_david.dam.City_Courier.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
+import com.tfg_david.dam.City_Courier.model.Asignacion;
 import com.tfg_david.dam.City_Courier.model.Disponibilidad;
 import com.tfg_david.dam.City_Courier.model.Repartidor;
+import com.tfg_david.dam.City_Courier.repository.AsignacionRepository;
 import com.tfg_david.dam.City_Courier.repository.RepartidorRepository;
 import com.tfg_david.dam.City_Courier.service.base.BaseService;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class RepartidorService extends BaseService<Repartidor, String, RepartidorRepository> {
 
+	
+	private final AsignacionRepository repoAsig; 
+	
 	public Long count() {
 		
 		return repo.count();
@@ -22,6 +31,27 @@ public class RepartidorService extends BaseService<Repartidor, String, Repartido
 	public Long countByEstado (Disponibilidad estado) {
 		
 		return repo.countByEstado(estado);
+		
+	}
+	
+	
+	@Transactional
+	public void deleteRepartidor(String dni) {
+		
+		Optional<Repartidor> repartidor; 
+		repartidor = repo.findById(dni);
+		List<Asignacion> asignacionesRepartidor; 
+		
+		if (repartidor.isPresent()) {
+			
+			
+		asignacionesRepartidor = repoAsig.findByRepartidor(repartidor.get());
+		
+		repoAsig.deleteAll(asignacionesRepartidor);
+		
+		repo.deleteById(dni);
+		
+		}
 		
 	}
 	
